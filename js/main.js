f7bd2b1b5df3c8236829d14e34a3959bc0aa315c
Trey Piepmeier <trey@treypiepmeier.com>
2013-01-06 01:03:05 -0500

    Style overhaul
    Reverse order for episodes, removed background, removed JS, tweaked icon/image.
    
    Fonts inspired by http://weblog.bocoup.com

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$('section[data-type="background"]').each(function() {
-    var $t = $(this);
-    $(window).scroll(function() {
-        var y = -($(window).scrollTop() / $t.data('speed'));
-        var coords = '50% ' + y + 'px';
-        $t.css({ backgroundPosition: coords });
-    });
-});
