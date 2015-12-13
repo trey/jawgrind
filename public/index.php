60bc8e4039d439952fca16f3dee31df4c759e417
Trey Piepmeier <trey@treypiepmeier.com>
2015-12-13 13:23:02 -0500

    Updating Gauges tracking code.

--- public/index.php
+++ public/index.php
@@ -93,7 +93,8 @@
                 t.async = true;
                 t.id    = 'gauges-tracker';
                 t.setAttribute('data-site-id', '51939591108d7b7cb500017d');
-                t.src = '//secure.gaug.es/track.js';
+                t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
+                t.src = 'https://track.gaug.es/track.js';
                 var s = document.getElementsByTagName('script')[0];
                 s.parentNode.insertBefore(t, s);
             })();
